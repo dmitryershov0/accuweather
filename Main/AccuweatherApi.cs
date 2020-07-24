f522ecd0a3bc9f4dd6f2287fe077de36dc2aa586
Dmitrii Ershov <dmitryershov0@gmail.com>
2020-07-24 04:41:09 +0300

    Initial project + add LocationsApi

---
+++ Main/AccuweatherApi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Accuweather.Locations;
+
+namespace Accuweather
+{
+    /// <summary>
+    /// Accuweather Api client
+    /// </summary>
+    public class AccuweatherApi : IAccuweatherApi
+    {
+        private readonly string _apiKey;
+        private readonly string _language;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="apiKey">Accuweather api key</param>
+        /// <param name="language">Language</param>
+        public AccuweatherApi(string apiKey, string language="en-us")
+        {
+            _apiKey = apiKey;
+            _language = language;
+        }
+        private ILocationsApi _locations;
+
+        /// <summary>
+        /// The Locations API allows users to get a location key for their desired location.
+        ///  This location key will then be used in the other APIs to retrieve weather data.
+        /// </summary>
+        /// <returns></returns>
+        public ILocationsApi Locations => _locations ?? (_locations = new LocationsApi(_apiKey, _language));
+        
+    }
+}
---
+++ Main/IAccuweatherApi.cs
@@ -0,0 +1,17 @@
+﻿using Accuweather.Locations;
+
+namespace Accuweather
+{
+    /// <summary>
+    /// Accuweather Api interface
+    /// </summary>
+    public interface IAccuweatherApi
+    {
+        /// <summary>
+        /// The Locations API allows users to get a location key for their desired location.
+        /// This location key will then be used in the other APIs to retrieve weather data.
+        /// </summary>
+        /// <value></value>
+        ILocationsApi Locations {get;}
+    }
+}
