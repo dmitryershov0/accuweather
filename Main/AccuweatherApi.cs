eaa2bcccae35f5ed21421f5ac3f90e22574f3f82
Dmitrii Ershov <dmitryershov0@gmail.com>
2020-07-26 11:01:44 +0300

    Add  Forecast, Indices

--- Main/AccuweatherApi.cs
+++ Main/AccuweatherApi.cs
@@ -1,43 +1,46 @@
 ﻿using System;
 using Accuweather.Locations;
 using Accuweather.Current;
+using Accuweather.Indices;
+using Accuweather.Forecast;
 
 namespace Accuweather
 {
-    /// <summary>
-    /// Accuweather Api client
-    /// </summary>
-    public class AccuweatherApi : IAccuweatherApi
-    {
-        private readonly string _apiKey;
-        private readonly string _language;
+	/// <inheritdoc cref="IAccuweatherApi"/>
+	public class AccuweatherApi : IAccuweatherApi
+	{
+		private readonly string _apiKey;
+		private readonly string _language;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="apiKey">Accuweather api key</param>
-        /// <param name="language">Language</param>
-        public AccuweatherApi(string apiKey, string language="en-us")
-        {
-            _apiKey = apiKey;
-            _language = language;
-        }
-        private ILocationsApi _locations;
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="apiKey">Accuweather api key</param>
+		/// <param name="language">Language</param>
+		public AccuweatherApi(string apiKey, string language = "en-us")
+		{
+			_apiKey = apiKey;
+			_language = language;
+		}
+		private ILocationsApi _locations;
 
-        /// <summary>
-        /// The Locations API allows users to get a location key for their desired location.
-        ///  This location key will then be used in the other APIs to retrieve weather data.
-        /// </summary>
-        /// <returns></returns>
-        public ILocationsApi Locations => _locations ?? (_locations = new LocationsApi(_apiKey, _language));
+		///<inheritdoc cref="IAccuweatherApi.Locations"/>
+		public ILocationsApi Locations => _locations ?? (_locations = new LocationsApi(_apiKey, _language));
 
-        private ICurrentConditions _currentConditions;
+		private ICurrentConditions _currentConditions;
 
-        /// <summary>
-        /// Get Current Conditions data for a specific location.
-        /// </summary>
-        /// <value></value>
-        public ICurrentConditions CurrentConditions => _currentConditions ?? (_currentConditions = new CurrentConditions(_apiKey, _language));
-        
-    }
+		///<inheritdoc cref="IAccuweatherApi.CurrentConditions"/>
+		public ICurrentConditions CurrentConditions => _currentConditions ?? (_currentConditions = new CurrentConditions(_apiKey, _language));
+
+		private IForecast _forecast;
+
+		///<inheritdoc cref="IAccuweatherApi.Forecast"/>
+		public IForecast Forecast => _forecast ?? (_forecast = new Forecast.Forecast(_apiKey, _language));
+
+		private IIndices _indices;
+
+		///<inheritdoc cref="IAccuweatherApi.Indices"/>
+		public IIndices Indices => _indices ?? (_indices = new Indices.Indices(_apiKey, _language));
+
+	}
 }
--- Main/IAccuweatherApi.cs
+++ Main/IAccuweatherApi.cs
@@ -1,24 +1,38 @@
 ﻿using Accuweather.Locations;
 using Accuweather.Current;
+using Accuweather.Forecast;
+using Accuweather.Indices;
 
 namespace Accuweather
 {
-    /// <summary>
-    /// Accuweather Api interface
-    /// </summary>
-    public interface IAccuweatherApi
-    {
-        /// <summary>
-        /// The Locations API allows users to get a location key for their desired location.
-        /// This location key will then be used in the other APIs to retrieve weather data.
-        /// </summary>
-        /// <value></value>
-        ILocationsApi Locations {get;}
+	/// <summary>
+	/// Accuweather Api
+	/// </summary>
+	public interface IAccuweatherApi
+	{
+		/// <summary>
+		/// The Locations API allows users to get a location key for their desired location.
+		/// This location key will then be used in the other APIs to retrieve weather data.
+		/// </summary>
+		/// <value></value>
+		ILocationsApi Locations { get; }
 
-        /// <summary>
-        /// Get Current Conditions data for a specific location.
-        /// </summary>
-        /// <value></value>
-        ICurrentConditions CurrentConditions {get;}
-    }
+		/// <summary>
+		/// Get Current Conditions data for a specific location.
+		/// </summary>
+		/// <value></value>
+		ICurrentConditions CurrentConditions { get; }
+
+		/// <summary>
+		/// Get forecast information for a specific location.
+		/// </summary>
+		/// <value></value>
+		IForecast Forecast { get; }
+
+		/// <summary>
+		/// Get indices information for a specific location.
+		/// </summary>
+		/// <value></value>
+		IIndices Indices { get; }
+	}
 }
